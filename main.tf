71de69ac55ba997ab56bcb0538977634fa40965d
unknown <dasariravid145@gmail.com>
2026-05-19 11:28:44 +0530

    action

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.subnet_public[count.index]
   availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
   tags = merge(local.common_tags,
         {
             Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
